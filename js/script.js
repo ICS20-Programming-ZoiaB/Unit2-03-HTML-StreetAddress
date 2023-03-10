42904358c788002176de9e1fc09ef6900609cd6f
ZoiaBaran <124380748+ZoiaBaran@users.noreply.github.com>
2023-03-10 23:33:00 +0000

    changed age to number

--- js/script.js
+++ js/script.js
@@ -5,13 +5,13 @@
 // This file contains the JS functions for index.html
 
 function enterClicked () {
-  // this function gets user's first name and age and displays it back to user
+  // this function gets user's street name and number and displays it back to user
 
-  // get first name from text field
+  // get street name from text field
   let streetName = document.getElementById("street-name").value
-	// get age from text field and cast it to integer
+	// get street number from text field and cast it to integer
   let streetNumber = parseInt(document.getElementById("street-number").value)
 
-  // display name and age back to user
+  // display street name and street number back to user
   document.getElementById('user-info').innerHTML = "Your street name is " + streetName + " and your street number is " + streetNumber + "!"
 }
